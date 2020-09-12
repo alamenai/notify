bd195073529e0c4d00b4bb3040e1265c8dc50df9
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-12 02:54:17 -0700

    :tada: Implement unit test

---
+++ src/components/Notification/Header/Icon/index.test.js
@@ -0,0 +1,35 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import Enzyme, { shallow } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import Icon, { setIcon } from "./index"
+import { Wrapper } from "./Styled"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("<Icon/>", () => {
+
+    const props = {
+        type: "info",
+    }
+
+    it('renders children when passed in', () => {
+        const { type } = props
+
+        const Element = <Wrapper type={props.type}>
+            <FontAwesomeIcon
+                icon={setIcon(type)}
+                size="2x" />
+        </Wrapper>;
+
+        const wrapper = shallow(<Icon />);
+        expect(wrapper.contains(Element)).toBeTruthy();
+    });
+
+    test("has a valid snapshot", () => {
+        const component = renderer.create(<Icon />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+})
