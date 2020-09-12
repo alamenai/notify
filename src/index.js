bf2bc3976cc97de478e32d0df44c8b05fdd6bea8
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-12 03:08:21 -0700

    :cyclone: Replace defautl App component with Notification

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Notification from "./components/Notification";
+
+
+const data = {
+    title: "System updates",
+    subtitle: "This is related to your OS",
+    message: "Your operating system is missing some files, please reboot your PC and upgrade your system."
+}
+
+const style = {
+    rounded: true,
+    animation: "left2right",
+    duration: 1
+}
+
+const action = {
+    name: "Reboot",
+    event: () => alert("Reboot")
+}
+
+const type = "warning";
+
+const notification = { type, data, style, action };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Notification {...notification} darkmode />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
