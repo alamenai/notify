e7f5829e1d8a5a6997a9d0a0b57deaa7df2e4eea
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-12 02:49:28 -0700

    :tada: Create styles for the component

---
+++ src/components/Notification/Body/Styled.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+       margin-top:0px;
+       max-width: fit-content;
+`
+export const Text = styled.p`
+       color:${({ darkmode }) => setColor(darkmode)};
+       font-size:0.78rem;
+       font-weight:500;
+       font-family:'Arial';
+       margin-top:0;
+       margin-left:5px;
+       text-align:justify;
+       line-height:1.4rem;
+`
+const setColor = (darkmode) => {
+    if (darkmode) {
+        return 'rgba(140,140,140)'
+    }
+    return 'rgba(60,60,60)'
+}
